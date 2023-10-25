009e151d56a89e0984db7893e416450282bf37a9
Aanal Trivedi <aanal.trivedi@innodeed.com>
2023-10-25 12:25:13 +0530

    branch changed

--- Package.swift
+++ Package.swift
@@ -13,8 +13,9 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.1.0")),
-    ],
+//        .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.1.0")),
+        .package(url: "https://github.com/ashleymills/Reachability.swift", branch: "master")
+  ],
     
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
