d86c813a3610809d4e4345c7da9253ad4a8f7120
Aanal Trivedi <aanal.trivedi@innodeed.com>
2023-10-25 11:25:32 +0530

    package added

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Math_Alamofire_Package",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Math_Alamofire_Package",
+            targets: ["Math_Alamofire_Package"]),
+    ],
+    
+    dependencies: [
+        .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.1.0")),
+    ],
+    
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Math_Alamofire_Package",
+            dependencies: [
+//                .target(name: "Math_Alamofire"),
+//                .product(name: "Reachability.swift", package: "Reachability.swift")
+                .product(name: "Reachability", package: "Reachability.swift"),
+
+            ]),
+
+        
+            .binaryTarget(
+                name: "Math_Alamofire",
+                path: "./Sources/Math_Alamofire.xcframework"
+            )
+    ]
+)
---
+++ Sources/Math_Alamofire.xcframework/ios-arm64_x86_64-simulator/Math_Alamofire.framework/Headers/Math_Alamofire.h
@@ -0,0 +1,18 @@
+//
+//  Math_Alamofire.h
+//  Math_Alamofire
+//
+//  Created by SAPPHIRE on 25/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Math_Alamofire.
+FOUNDATION_EXPORT double Math_AlamofireVersionNumber;
+
+//! Project version string for Math_Alamofire.
+FOUNDATION_EXPORT const unsigned char Math_AlamofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Math_Alamofire/PublicHeader.h>
+
+
